6bdf98395d28618e51133381e0225c5c8674f951
mug913 <brad.alexander913@gmail.com>
2020-01-18 19:13:24 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
 
     validates :title, presence: true
     validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
-    validates :content, length: {mimimum: 2}
+    validates :content, length: {minimum: 100}
 end
